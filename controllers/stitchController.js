0108f7bf23ede3af1fc61f446f1101302d87a10e
ericsmith92 <ericsmth884@gmail.com>
2020-12-23 17:33:50 -0500

    resizing reintroduced

--- controllers/stitchController.js
+++ controllers/stitchController.js
@@ -10,34 +10,35 @@ exports.stitchImages = async (req, res) => {
   .then( data => {
 
 
-    /*
+    
     let pixelX = 0;
     let pixelY = 0;
     
     for(let i = 0; i < data.length - 1; i++){
-      data[0].composite(data[i + 1], pixelX, pixelY);
+      data[0].composite(data[i + 1].resize(400, 400), pixelX, pixelY);
 
-      pixelX = (i + 1) % 3 !== 0 ? pixelX + 300 : 0;
+      pixelX = (i + 1) % 3 !== 0 ? pixelX + 400 : 0;
 
       if( (i + 1) % 3 === 0){
-        pixelY = pixelY + 300;
+        pixelY = pixelY + 400;
       }
     }
-    */
-
-   data[0].composite(data[1],0, 0);
-   data[0].composite(data[2],400, 0);
-   data[0].composite(data[3],800, 0);
-   data[0].composite(data[4],0, 400);
-   data[0].composite(data[5],400, 400);
-   data[0].composite(data[6],800, 400);
-   data[0].composite(data[7],0, 800);
-   data[0].composite(data[8],400, 800);
-   data[0].composite(data[9],800, 800);
-   data[0].composite(data[10],0, 1200);
-   data[0].composite(data[11],400, 1200);
-   data[0].composite(data[12],800, 1200);
     
+   /*
+   data[0].composite(data[1].resize(400, 400),0, 0);
+   data[0].composite(data[2].resize(400, 400),400, 0);
+   data[0].composite(data[3].resize(400, 400),800, 0);
+   data[0].composite(data[4].resize(400, 400),0, 400);
+   data[0].composite(data[5].resize(400, 400),400, 400);
+   data[0].composite(data[6].resize(400, 400),800, 400);
+   data[0].composite(data[7].resize(400, 400),0, 800);
+   data[0].composite(data[8].resize(400, 400),400, 800);
+   data[0].composite(data[9].resize(400, 400),800, 800);
+   data[0].composite(data[10].resize(400, 400),0, 1200);
+   data[0].composite(data[11].resize(400, 400),400, 1200);
+   data[0].composite(data[12].resize(400, 400),800, 1200);
+   */
+
     const term = req.body.term;
     const stitchedImagePath = `public/images/user-images/${term}/${Date.now()}.png`
     
